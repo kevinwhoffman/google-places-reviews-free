fabc2bdea2f025db91d659529f128978d27931ef
Devin Walker <devin@wordimpress.com>
2016-06-02 15:08:39 -0700

    New gulp file for translation files

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+/**
+ * Google Places Reviews Gulp
+ */
+
+//Necessary packages
+var gulp = require('gulp');
+var wpPot = require('gulp-wp-pot');
+
+//POT file task
+gulp.task('pot', function () {
+    return gulp.src('**/*.php')
+        .pipe(sort())
+        .pipe(wpPot({
+            package: 'Google Places Reviews Pro',
+            domain: 'gpr',
+            destFile: 'gpr.pot',
+            lastTranslator: '',
+            team: 'WordImpress <info@wordimpress.com>'
+        }))
+        .pipe(gulp.dest('lang'));
+});
+
+//Default task
+gulp.task('default', function () {
+    gulp.start('pot');
+});
